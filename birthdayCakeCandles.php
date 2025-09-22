307f4f47d5afbd9b210bb630e56488da205ef5a3
Bukunmi <tumiseadeyinka@gmail.com>
2025-09-22 09:25:29 +0100

    Added Solution for Birthday Cake Candles Problem

---
+++ birthdayCakeCandles.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles($candles) {
+    // Write your code here
+    $tallest = max($candles);
+    $counttallest = 0;
+    foreach($candles as $candle){
+        if($candle == $tallest){
+            $counttallest++;
+        }
+    }
+    $count = array_count_values($candles);
+    
+    return $counttallest;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$candles_count = intval(trim(fgets(STDIN)));
+
+$candles_temp = rtrim(fgets(STDIN));
+
+$candles = array_map('intval', preg_split('/ /', $candles_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = birthdayCakeCandles($candles);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
