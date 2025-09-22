0c213791e8c7a418ae149975818197b47ecfcd7d
Bukunmi <tumiseadeyinka@gmail.com>
2025-09-22 16:54:28 +0100

    Added Solution to Time Conversion Problem

---
+++ timeConversion.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion($s) {
+    // Write your code here
+    $dt = DateTime::createFromFormat("h:i:sA", $s);
+    $s = $dt->format("H:i:s");
+    return $s; 
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$s = rtrim(fgets(STDIN), "\r\n");
+
+$result = timeConversion($s);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
