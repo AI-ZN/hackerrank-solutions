6eafb583ff815886954707a21809fb27f47b6b9a
Bukunmi <tumiseadeyinka@gmail.com>
2025-09-29 09:57:11 +0100

    Last Task On friday Solution

--- Hackaton/hackaton1.php
+++ Hackaton/hackaton1.php
@@ -10,21 +10,56 @@ function RaceTraining($i,$j){
     while($Beck_15 < $Track && $Romeo_7 < $Track){
         $Beck_15 += 2;
         $Romeo_7 += 1;
-            if(($Beck_15 == $Romeo_7) && $Beck_15 % 6 == 0 ){
-                echo "Draw => Both Met at CheckPoint " . ($Beck_15 / 6) . "<br>";
+            if(($Beck_15 == $Romeo_7) ){
+                echo "Draw => Both Met at Checkpoint " . ($Beck_15 / 6) . "<br>";
             }
-            if($Beck_15 >=$Track && $Romeo_7 >= $Track){
-                echo "It was a Tie";
-            }elseif($Beck_15 >= $Track && $Romeo_7 < $Track ){
-                echo "Beck Won";
-            }elseif($Beck_15 < $Track && $Romeo_7 >= $Track){
-                echo "Romeo Won";
-            }
+            
         
         }
     }
+    if($Beck_15 >=$Track && $Romeo_7 >= $Track){
+                echo "It was a Tie<br>";
+            }elseif($Beck_15 >= $Track && $Romeo_7 < $Track ){
+                echo "Beck Won The race<br>";
+            }elseif($Beck_15 < $Track && $Romeo_7 >= $Track){
+                echo "Romeo Won The race<br>";
+     }
     
 }
-RaceTraining(1, 9);
+
+function RomeoWin($i){
+    if ($i <= 0 || $i >=1001) {
+    echo "Invalid starting position";
+    }else{
+        $Track = 6000;
+        $Beck_15 = $i * 6;
+        
+        for ($j = $i + 1; $j <= 1001; $j+=1){
+            $Romeo_7 = $j * 6;
+            $beck = $Beck_15;
+            $romeo = $Romeo_7;
+            $draw = false;
+                while($romeo < $Track && $beck < $Track){
+                    $romeo +=1;
+                    $beck +=2;
+
+                    if($romeo == $beck){
+                        $draw = true;
+                        break;
+                    }
+                    
+                }
+            
+            if(!$draw && $romeo >= $Track &&  $beck < $Track){
+                
+                    echo "The minimum checkpoint for Romeo when Beck is " . $i . " is " . $j;
+                    break;
+            }
+        }
+    }
+}
+
+RaceTraining(1,6);
+RomeoWin(5);
 
 ?>
